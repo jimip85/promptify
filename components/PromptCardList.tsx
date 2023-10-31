69125ca064b04435b00f414041c153d33a533ae1
Jimi <122390267+jimip85@users.noreply.github.com>
2023-10-31 13:26:57 -0400

    display most recent posts

--- components/PromptCardList.tsx
+++ components/PromptCardList.tsx
@@ -3,30 +3,33 @@ import PromptCard from "./PromptCard";
 import { Post } from "@app/types";
 
 interface PromptCardListProps {
-    data: Post[];
-    handleTagClick: (tagName: string) => void;
-  }
-  
-  const PromptCardList: React.FC<PromptCardListProps> = ({
-    data,
-    handleTagClick,
-  }) => {
-    return (
-      <div className="mt-8 sm:columns-2 sm:gap-6 xl:columns-3 ">
-        <TransitionGroup>
-          {data.map((post, index) => (
-            <CSSTransition key={`${post.creator._id}-${index}`} timeout={400} classNames="fade">
-              <div className="mb-6">
-                <PromptCard
-                  post={post}
-                  handleTagClick={handleTagClick}
-                />
-              </div>
-            </CSSTransition>
-          ))}
-        </TransitionGroup>
-      </div>
-    );
-  };
+  data: Post[];
+  handleTagClick: (tagName: string) => void;
+}
 
-  export default PromptCardList;
+const PromptCardList: React.FC<PromptCardListProps> = ({
+  data,
+  handleTagClick,
+}) => {
+  const reversedData = [...data].reverse();
+
+  return (
+    <div className="mt-8 sm:columns-2 sm:gap-6 xl:columns-3 ">
+      <TransitionGroup>
+        {reversedData.map((post, index) => (
+          <CSSTransition
+            key={`${post.creator._id}-${index}`}
+            timeout={400}
+            classNames="fade"
+          >
+            <div className="mb-6">
+              <PromptCard post={post} handleTagClick={handleTagClick} />
+            </div>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default PromptCardList;
