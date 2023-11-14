83622ccc33c0862d759c8e8dd0a00a75e16392fc
Jimi <122390267+jimip85@users.noreply.github.com>
2023-11-14 16:50:20 -0500

    openai streaming

--- app/api/openai/openai.ts
+++ app/api/openai/openai.ts
@@ -1,43 +1,90 @@
- export const fetchData = async (content:string) => {
-    const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+/* export const fetchData = async (content: string) => {
+  const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 
-    if (!apiKey) {
-      console.error("OpenAI API key is missing.");
-      return;
+  if (!apiKey) {
+    console.error("OpenAI API key is missing.");
+    return;
+  }
+
+  const apiUrl = "https://api.openai.com/v1/chat/completions";
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: content,
+          },
+        ],
+        stream: true,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Failed to fetch data: ${response.status} - ${errorText}`);
     }
-  
-    const apiUrl = "https://api.openai.com/v1/chat/completions";
-  
-    try {
-      const response = await fetch(apiUrl, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          messages: [
-            {
-              role: "user",
-              content: content,
-            },
-          ],
-        }),
-      });
-  
-      if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(`Failed to fetch data: ${response.status} - ${errorText}`);
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    let resultText = "";
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
       }
-      
-  
-      const data = await response.json();
-      console.log(data.choices[0].message.content);
-      return data.choices[0].message.content;
-    } catch (error) {
-      console.error("Error fetching data:", error.message);
-    }
-  };
+      const decodedChunk = decoder.decode(value);
+      const lines = decodedChunk.split("\n");
+      const parsedLines = lines.map(line => line.replace(/^data: /, "").trim()).filter(line => line !== "" && line !== "[DONE]").map(line => JSON.parse(line));
 
-  
+      for (const parsedLine of parsedLines) {
+        const { choices } = parsedLine;
+        const { delta } = choices[0];
+        const { content } = delta;
+        if (content) {
+          resultText += content;
+          console.log(content);
+        }
+      }
+    }
+    return resultText;
+
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+  }
+}; */
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({apiKey:process.env.NEXT_PUBLIC_OPENAI_API_KEY, dangerouslyAllowBrowser: true,});
+
+export const fetchData = async ({ content, setCompletion }: {
+  content: string;
+  setCompletion: (response: string) => void;
+}) => {
+  let finalResponse = "";
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: content },
+    ],
+    stream: true,
+  });
+
+  for await (const chunk of response) {
+    finalResponse += chunk.choices[0].delta.content;
+    console.log(finalResponse);
+    setCompletion(finalResponse);
+  }
+}
+
+
